85bac4dbd01686a70cc1841172e9f83be1775ad5
Gabriel Virga <gfelixvirga@mtb.com>
2020-12-02 12:47:05 -0500

    challenges

---
+++ Advent of Code 2020/day1.py
@@ -0,0 +1,39 @@
+array = []
+
+with open('day1input.txt') as f:
+    for line in f:
+        array.append(int(line))
+
+
+left = 0
+right = len(array)-1
+array.sort()
+
+#Two Sum
+while left < right:
+    sum = array[right] + array[left] 
+    #print(f"{array[right]} + {array[left]} = {sum}")
+    if sum == 2020:
+        print(array[right] * array[left])
+        break
+
+    if sum > 2020:
+        right -= 1
+    else:
+        left += 1
+
+# Three Sum
+for i in range(len(array) - 2):
+    left = i + 1
+    right = len(array)-1
+    while left < right:
+        sum = array[i] + array[right] + array[left] 
+        #print(f"{array[i]} + {array[right]} + {array[left]} = {sum}")
+        if sum == 2020:
+            print(array[i] * array[right] * array[left])
+            break
+
+        if sum > 2020:
+            right -= 1
+        elif sum < 2020:
+            left += 1
---
+++ Advent of Code 2020/day2.py
@@ -0,0 +1,22 @@
+import re
+
+counter = 0
+with open('day2input.txt') as f:
+    for line in f:
+        mininum, maximum, char, password = re.split('-| |: ', line)
+        if int(mininum) <= password.count(char) <= int(maximum):
+            #print(f" {int(mininum)} <= {password.count(char)} <= {int(maximum)}")
+            counter += 1
+
+print(f"part one: {counter}")
+
+counter = 0
+with open('day2input.txt') as f:
+    for line in f:
+        position_one, position_two, char, password = re.split('-| |: ', line)
+        if char == password[int(position_one)-1] and char !=  password[int(position_two)-1]:
+            counter += 1
+        elif char != password[int(position_one)-1] and char ==  password[int(position_two)-1]:
+            counter += 1
+        
+print(f"part Two: {counter}")
