26e4255345050cac572d95b111bddca477738584
Gabriel Virga <gfelixvirga@mtb.com>
2020-12-06 10:28:21 -0500

    day6

--- Advent of Code 2020/day6.py
+++ Advent of Code 2020/day6.py
@@ -20,3 +20,15 @@ for line in  file.read().split("\n\n"):
             questions_count += 1    
 print("Part Two")
 print(f"Questions Count: {questions_count}\n")
+
+
+# Part Two using set()
+questions_count = 0
+file = open('day6input.txt',mode='r')
+for line in  file.read().split("\n\n"):
+    line = line.split("\n")
+    for char in set(s for x in line for s in x):
+        if all(char in question for question in line):
+            questions_count += 1    
+print("Part Two")
+print(f"Questions Count: {questions_count}\n")
