23ed1d7d450a5a84f4221efd014d0181d7a3c687
Gabriel Virga <gfelixvirga@mtb.com>
2020-12-11 21:12:31 -0500

    push

--- Advent of Code 2020/day10.py
+++ Advent of Code 2020/day10.py
@@ -5,6 +5,7 @@ diff = {
     1: 0,
     3: 0
 }
+
 nums.append(0)
 nums.append(max(nums) + 3)
 
@@ -22,4 +23,26 @@ print(f"Part One: {diff[1] * diff[3]}")
 
 
 # Part Two
-#¯\_(ツ)_/¯
+
+def valid(array):
+    i = 1
+    jolts_helper = 0
+    while i <= max(array):
+        jolts_helper += 1
+        if i in array: 
+            jolts_helper = 0
+        if jolts_helper > 3:
+            return False
+        i += 1
+    return True
+
+result = 0
+counter = 0
+nums.sort()
+for i in range(1,len(nums)):
+    print(nums[:i] + nums[i+1:])
+    if valid(nums[:i] + nums[i+1:]):
+        result += 1
+        print(result)
+
+print(result)
--- Advent of Code 2020/day11.py
+++ Advent of Code 2020/day11.py
@@ -15,38 +15,37 @@ def count_adj(x, y, max_row, max_col):
         sum_y = sum([y,dy])
         #print(f"[{sum_x}][{sum_y}]")
         if sum_x < 0  or sum_y < 0: 
-            print(f"0")
+            # print(f"0")
             continue
         if sum_x > max_row or sum_y > max_col:
-            print(f"0")
+            # print(f"0")
             continue
-        print(f"[{sum_x}][{sum_y}] {seats[sum_x][sum_y]}")
+        # print(f"[{sum_x}][{sum_y}] {seats[sum_x][sum_y]}")
         if seats[sum_x][sum_y] == "#":
-            print(f" #")
+            # print(f" #")
             cockupy += 1
     # print(cockupy)
     return cockupy
 
 # Everyone came in to seat
 for _ in range(2):
-    seat_helper = copy.deepcopy(seats)
+    seats_helper = copy.deepcopy(seats)
     visualize(seats)
     for row in range(len(seats)):
         for col in range(len(seats[0])):
             if seats[row][col] == ".": continue
             count = count_adj(row,col,len(seats)-1,len(seats[0])-1)
-            print(f"r {row}c {col} count {count}")
+            # print(f"r {row}c {col} count {count}")
             if seats[row][col] == "#" and count > 3:
-                seats[row][col] = "L"
-
+                seats_helper[row][col] = "L"
             if count == 0 and seats[row][col] == "L":
-                seat_helper[row][col] = "#"
-    # visualize(seats)
-    # print("yo")
-    # visualize(seat_helper)
+                seats_helper[row][col] = "#"
+    visualize(seats)
+    print("yo")
+    visualize(seats_helper)
     print('--end-')
     
-    seats = copy.deepcopy(seat_helper)
+    seats = copy.deepcopy(seats_helper)
 
 
 
