1594bb93d9f994bb15295a6518186fed2764adaa
Justina Arriaza Virga <jarri@Justinas-MacBook-Pro.local>
2020-12-11 22:15:20 -0500

    day 11 part one

---
+++ Advent of Code 2020/day11.py
@@ -0,0 +1,46 @@
+import copy
+file = open('day11input.txt',mode='r')
+seats = [list(line) for line in file.read().split("\n")]
+directions = [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]
+
+def visualize(matrix):
+    occupied_total = 0
+    for row in range(len(matrix)):
+        print(''.join(matrix[row]))
+        occupied_total += matrix[row].count('#')
+    print(f"Part One: {occupied_total}")
+
+def count_adj(x, y, max_row, max_col):
+    cockupy = 0
+    for direction in directions:
+        dx, dy = direction
+        sum_x = sum([x,dx])
+        sum_y = sum([y,dy])
+        if sum_x < 0  or sum_y < 0: 
+            continue
+        if sum_x > max_row or sum_y > max_col:
+            continue
+        if seats[sum_x][sum_y] == "#":
+            cockupy += 1
+    return cockupy
+
+# Everyone taking seats
+while True:
+    seats_helper = copy.deepcopy(seats)
+    # visualize(seats)
+    for row in range(len(seats)):
+        for col in range(len(seats[0])):
+            if seats[row][col] == ".": continue
+            count = count_adj(row,col,len(seats)-1,len(seats[0])-1)
+            if seats[row][col] == "#" and count > 3:
+                seats_helper[row][col] = "L"
+            if count == 0 and seats[row][col] == "L":
+                seats_helper[row][col] = "#"
+
+    if seats_helper == seats:
+        visualize(seats)
+        break
+    seats = copy.deepcopy(seats_helper)
+
+
+
