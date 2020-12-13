0bd6c8ea5ae88e2be861dbad90a45e10b9a1c3dd
Gabriel Virga <bielfelix@gmail.com>
2020-12-12 22:22:19 -0500

    day12

--- Advent of Code 2020/day12.py
+++ Advent of Code 2020/day12.py
@@ -30,46 +30,43 @@ for line in file.read().split("\n"):
                 facing_index = 3
         facing = facing_array[facing_index]
 
-    print(coordinates)
-print(max(coordinates['N'],coordinates['S'])+ 
+print(max(coordinates['N'],coordinates['S']) + 
         max(coordinates['E'],coordinates['W'])
         )
 
 facing = 'E'
 waypoint = {
-    'N': 10,
-    'E': 1,
-    'S': -10,
-    'W': -1
+    'N': 1,
+    'E': 10,
+}
+coordinates = {
+    'N': 0,
+    'E': 0
 }
 
-# Part Two:
+# Part Two - PLAGIARISM Alert:
 file = open('day12input.txt',mode='r')
 for line in file.read().split("\n"):
     action, value = [line[:1],int(line[1:])]
-    print(f"{action}{value} - Facing: {facing} ")
+    print(f"{action}{value} waypoint = {waypoint}")
+    if action in 'SW':
+        waypoint[opposite[action]] -= value
+    if action in "NE":
+        waypoint[action] += value
+    elif action in "F":
+        coordinates['N'] += waypoint['N'] * value
+        coordinates['E'] += waypoint['E'] * value
 
-    if action in "F":
-        coordinates[facing] += value
-        coordinates[opposite[facing]] -= value
-    elif action in "NESW":
-        coordinates[action] += value
-        coordinates[opposite[action]] -= value
-    elif action in "RL":
-        facing_array = list(coordinates.keys())
-        facing_index = facing_array.index(facing)
-        for i in range(opposite[value]):
-            facing_index += 1 * opposite[action]
-            print(f"b {facing_index}")
-            if facing_index > 3:
-                facing_index = 0
-            if facing_index < 0:
-                facing_index = 3
-            print(facing_index)
-        print(f"originally {facing} turned {opposite[value]*opposite[action]}")
-        facing = facing_array[facing_index]
+    # I looked up how other did this and I thought this was cleaver! PLAGIARISM area
+    elif action in "L":
+        while value:
+            waypoint['N'], waypoint['E'] = waypoint['E'], -waypoint['N']
+            value -= 90
+    elif action in "R":
+        while value:
+            waypoint['N'], waypoint['E'] = -waypoint['E'], waypoint['N']
+            print(waypoint)
+            value -= 90
 
     print(coordinates)
-print(max(coordinates['N'],coordinates['S'])+ 
-        max(coordinates['E'],coordinates['W'])
-        )
+print(abs(coordinates['N']) + abs(coordinates['E']))
