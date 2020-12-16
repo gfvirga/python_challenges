bb4446df3280a3685e42967626b9e6d09eb2162a
Gabriel Virga <gfelixvirga@mtb.com>
2020-12-15 19:45:08 -0500

    day15

---
+++ Advent of Code 2020/day15.py
@@ -0,0 +1,28 @@
+
+def speak(limit):
+    file = open('day15input.txt',mode='r')
+    nums = [int(x) for x in file.read().split(",")]
+    spoken = {}
+    for turn, spoke in enumerate(nums):
+        spoken[spoke] = [turn+1]
+
+    turn += 2
+
+    while turn < limit:
+        if spoke in spoken.keys():
+
+            if len(spoken[spoke]) > 1:
+                spoke = spoken[spoke][-2:][1] - spoken[spoke][-2:][0]    
+            else:
+                spoke = 0
+
+            if spoke in spoken.keys():
+                spoken[spoke].append(turn)
+            else:
+                spoken[spoke] = [turn]
+
+        turn += 1
+    return spoke
+
+print(f"Part One {speak(2021)}")
+print(f"Part Two {speak(30000001)}")
