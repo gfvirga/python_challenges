fd8cf36c0dc4c293b5c908b56ff4ca41f208aaeb
Gabriel Virga <gfvirga@gmail.com>
2020-12-18 21:40:12 -0500

    day 18

--- Advent of Code 2020/day18.py
+++ Advent of Code 2020/day18.py
@@ -2,23 +2,6 @@ import re
 file = open('day18input.txt',mode='r')
 equations = [e for e in file.read().split("\n")]
 
-def solve(line):
-    def doInner(inner):
-        while '+' in inner or '*' in inner:
-            inner = re.sub('^(\d+)\s*\+\s*(\d+)', lambda m: str(int(m.group(1)) + int(m.group(2))), inner)
-            inner = re.sub('^(\d+)\s*\*\s*(\d+)', lambda m: str(int(m.group(1)) * int(m.group(2))), inner)
-        # while '+' in inner:
-            # inner = re.sub('(\d+)\s*\+\s*(\d+)', lambda m: str(int(m.group(1)) + int(m.group(2))), inner)
-        # while '*' in inner:
-            # inner = re.sub('(\d+)\s*\*\s*(\d+)', lambda m: str(int(m.group(1)) * int(m.group(2))), inner)
-        return inner
-    while '(' in line:
-        def doExpr(match):
-            inner = match.group(1)
-            return doInner(inner)
-        line = re.sub(r'\(([^()]+)\)', doExpr, line)
-    return doInner(line)
-
 def calc(s):
     s = re.sub("(\(|\))", "", s)
     while "*" in s or "+" in s:
