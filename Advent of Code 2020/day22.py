784b87a742ec381d161038df1a7cdb8dcfd9daa9
Gabriel Virga <gfelixvirga@mtb.com>
2020-12-23 22:48:53 -0500

    day22

--- Advent of Code 2020/day22.py
+++ Advent of Code 2020/day22.py
@@ -1,18 +1,44 @@
 from copy import deepcopy
 file = open('./inputs/day22input.txt',mode='r')
 # Print players parses to {1: [9, 2, 6, 3, 1], 2: [5, 8, 4, 7, 10]}
-players = {int(k.split(' ')[1]): [int(x) for x in (v.split("\n")[1:] )] for k, v in [line.split(":") for line in file.read().split("\n\n")]}
-original_players = deepcopy(players)
+original_players = {int(k.split(' ')[1]): [int(x) for x in (v.split("\n")[1:] )] for k, v in [line.split(":") for line in file.read().split("\n\n")]}
+players = deepcopy(original_players)
 
-counter = 0
-while all(len(v) > 0 for v in players.values()):
-    highest = max(players[1][0], players[2][0])
-    k = 1 if (highest in players[1]) else 2
-    k_o = 1 if k == 2 else 2
-    value_helper = [players[k][0],players[k_o][0]]
-    for key in players.keys():
-        players[key].pop(0)
+# k == winner
+def game(players):
+    while all(len(v) > 0 for v in players.values()):
+        highest = max(players[1][0], players[2][0])
+        k = 1 if (highest in players[1]) else 2
+        k_o = 1 if k == 2 else 2
+        value_helper = [players[k][0],players[k_o][0]]
+        for key in players.keys():
+            players[key].pop(0)
 
-    players[k] += (value_helper)    
+        players[k] += (value_helper)    
+    return k, players
 
-print(f"Part One: {sum(x*(i+1) for i, x in enumerate(reversed(players[k])))}")
+k, result = game(players)
+print(f"Part One: {sum(x * i for i, x in enumerate(reversed(result[k]), 1))}")
+
+players = deepcopy(original_players)
+def recursive(players):
+
+    while all(len(v) > 0 for v in players.values()):
+        highest = max(players[1][0], players[2][0])
+        k = 1 if (highest in players[1]) else 2
+        k_o = 1 if k == 2 else 2
+        value_helper = {k: players[k][0], k_o: players[k_o][0]}
+        for key in players.keys():
+            players[key].pop(0)
+        if all(len(v) > 1 for v in players.values()):
+            # Verify if it needs to recursive
+            if len(players[k]) >= value_helper[k] and len(players[k_o]) >= value_helper[k_o]:
+                players_helper = {k: players[k][:value_helper[k]], k_o: players[k_o][:value_helper[k_o]]}
+                k, _ = recursive(players_helper)
+                k_o = 1 if k == 2 else 2
+                value_helper = {k: value_helper[k], k_o: value_helper[k_o]}
+        players[k] += (value_helper.values())    
+    return k, players
+
+k , result = recursive(players)
+print(f"Part Two: {sum(x * i for i, x in enumerate(reversed(result[k]), 1))}")
--- README.md
+++ README.md
@@ -5,10 +5,10 @@ This is my journey to Python.
 I am not a full time developer, however I love to automate and I enjoy to see my scripts to complete and run successfully. I decided to finally try out Python.
 
 ## 2020
-In 2020 the weirdest year ever, I had the pleasure to interview with a few of the FAAMNG companies and I realized that I am ahead on the kubernetes and system design game, but very behind on Python. So, I openeed a few books, completed some of the [30 Day of Python ](https://github.com/Asabeneh/30-Days-Of-Python), completed 100 challenges on leetcode, and AOC challenges.
+In 2020 the weirdest year ever, I had the pleasure to interview with a few of the FAAMNG companies and I realized that I am ahead on the kubernetes and system design game, but very behind on Python. So, I opened a few books, completed some of the [30 Day of Python ](https://github.com/Asabeneh/30-Days-Of-Python), completed 100 challenges on leetcode, and AOC challenges.
  
 ## Who Am I
-- Husband, Father of 2 little monsters, chicken owner, work on DIY house projects, gardener and lifter (push/pull/legs)
+- Husband, Father of 2 little monsters, chicken owner, work on DIY house projects, gardener and weight lifter (push/pull/legs)
 - I am a linux system administrator, working on devops initiatives and application containerization.
 - I am not a salesperson; I rarely persuade anyone into using Kubernetes or any cloud service, but I am always helpful within these topics!
 - I love linux and I love virtualization. Challenge me with an application that does not run in a container, I say "hold my beer" and I will work nonstop until that app is running in a container on kubernetes for extra points. 
